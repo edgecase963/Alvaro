37e81765c70fe4ddc7575efa71836c0ff7aeee24
edgecase963 <jacob.exypnos@gmail.com>
2020-05-16 11:47:12 -0400

    Beta 0.4.2
    Minor bug fixes + version fix

--- alvaro.py
+++ alvaro.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
-__version__ = "0.4.1 (Beta)"
+__version__ = "0.4.2 (Beta)"
 
 
 
